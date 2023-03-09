fc893cfe41225cde9e141f8024e0b436b430bbce
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2023-03-09 17:05:53 +0300

    Xresources

--- config.def.h
+++ config.def.h
@@ -3,21 +3,48 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+
+static char font[] = "FiraCode Nerd Font:size=10";
 static const char *fonts[] = {
-	"FiraCode Nerd Font:size=10"
+	font,
+	"FiraCode Nerd Font:size=10",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d8dee9", "#3b4252" },
-	[SchemeSel] = { "#88c0d0", "#3b4252" },
-	[SchemeOut] = { "#d8dee9", "#3b4252" },
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char selfgcolor[]  = "#eeeeee";
+static char selbgcolor[]  = "#005577";
+static char *colors[SchemeLast][2] = {
+ 	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+  [SchemeSel]  = { selfgcolor,  selbgcolor  },
+  [SchemeOut]  = { "#000000",   "#00ffff" },
 };
+
+
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 15;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        STRING, &font },
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "selbgcolor",  STRING, &selbgcolor },
+	{ "prompt",      STRING, &prompt },
+};
